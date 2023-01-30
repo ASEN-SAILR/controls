3b0342abc99abe6ce7ec6a90862e416a22314b4c
Caleb Bristol <caleb@cu-biot-3-10.203.139.0.int.colorado.edu>
2023-01-30 14:28:35 -0700

    added timestep calculation

--- controls/control_test.ino
+++ controls/control_test.ino
@@ -1,3 +1,13 @@
+/* Instructions for IMU/Magnetometer Use 
+-----------------------------------------
+- include "IMU_reader.h" in header
+- initialize an object of class IMU_reader for IMU, Magnetometer_reader for Magnetometer
+- recommend using accurate clock for time step (delta t between loop calls)
+- call read_Magnetometer() or read_IMU(time step)
+- profit
+-----------------------------------------
+*/
+
 #include"IMU_reader.h"
 
 // Class variables
@@ -8,6 +18,11 @@ int mode = 0; // 0 for IMU testing, 1 for Magnetometer Testing
 
 int timestep = 100;  //[ms]
 
+// More accurate delta t from system clock
+float dt = 0;
+unsigned long currentTime;
+unsigned long startTime;
+
 void setup(void) {
   // Initialize IMU and Magnetometer
   IMU.startup();
@@ -17,14 +32,25 @@ void setup(void) {
 void loop(void) {
   // Data collection variable
   float* vec;
+
+  // Time change adjustments
+  currentTime = millis();
+  if(!dt)
+    dt = timestep;
+  else
+    dt = currentTime - startTime;
+  startTime = millis();
+
   // IMU Mode
   if(mode)
   {
     // Read in IMU position data
-    vec = IMU.read_IMU(timestep);
+    vec = IMU.read_IMU(dt);
 
     // Print values to terminal
     Serial.print(vec[0]);
+    Serial.print(vec[1]);
+    Serial.print(vec[2]);
   }
   // Magnetometer Mode
   else
@@ -34,6 +60,8 @@ void loop(void) {
 
     // Print Data
     Serial.print(vec[0]);
+    Serial.print(vec[1]);
+    Serial.print(vec[2]);
   }
 
   // Time delay
