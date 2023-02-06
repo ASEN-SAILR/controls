cee27afe682de53101e250e1fd4364d1e653a73e
skywalkaa <49798726+skywalkaa@users.noreply.github.com>
2023-02-06 12:08:22 -0700

    updated for linux port name

--- hello_world_luke/extras/recieve_from_teensy.py
+++ hello_world_luke/extras/recieve_from_teensy.py
@@ -3,7 +3,7 @@ import serial
 import time
 
 if __name__ == '__main__':
-    ser = serial.Serial('COM12', 115200, timeout=1)
+    ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
     ser.reset_input_buffer()
     while True:
         if ser.in_waiting>0:
