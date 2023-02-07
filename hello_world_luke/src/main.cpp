b2f1ca9b5aebb1980c14982e4a5455e9248e63b8
skywalkaa <49798726+skywalkaa@users.noreply.github.com>
2023-02-07 14:22:59 -0700

    added blinkNTimes

--- hello_world_luke/src/main.cpp
+++ hello_world_luke/src/main.cpp
@@ -2,6 +2,17 @@
 
 const int led_pin = LED_BUILTIN;
 
+void blinkNTimes(int N){
+  for(int i=0; i<N; i++){
+
+    //set led high and low
+    digitalWrite(led_pin,HIGH);
+    delay(500);
+    digitalWrite(led_pin,LOW);
+    delay(500);
+  }
+}
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
@@ -14,37 +25,39 @@ void loop() {
 
   while(true){
 
-    Serial.write("Hello World\n");
-    // if (Serial.available()){
-    //   int len = Serial.available();
+    if (Serial.available()){
+      int len = Serial.available();
 
-    //   //read in bytes of data
-    //   for(int i=0; i<len; i++){
+      //read in bytes of data
+      for(int i=0; i<len; i++){
 
-    //     data[i] = Serial.read();
+        data[i] = Serial.read();
 
-    //     //set led high and low
-    //     digitalWrite(led_pin,HIGH);
-    //     delay(100);
-    //     digitalWrite(led_pin,LOW);
-    //   }
+        //set led high and low
+        digitalWrite(led_pin,HIGH);
+        delay(100);
+        digitalWrite(led_pin,LOW);
+        delay(100);
+      }
 
-    //   // indicate that the teensy is about to send bytes back with longer
-    //   //  LED pulses
-    //   for(int i=0; i<3; i++){
-    //     digitalWrite(led_pin,HIGH);
-    //     delay(500);
-    //     digitalWrite(led_pin,LOW);
-    //   }
+      // indicate that the teensy is about to send bytes back with longer
+      //  LED pulses
+      for(int i=0; i<3; i++){
+        digitalWrite(led_pin,HIGH);
+        delay(500);
+        digitalWrite(led_pin,LOW);
+        delay(500);
+      }
 
-    //   for(int i=0; i<len; i++){
-    //     Serial.write(data[i]);
+      for(int i=0; i<len; i++){
+        Serial.write(data[i]);
 
-    //     //turn led on and off
-    //     digitalWrite(led_pin,HIGH);
-    //     delay(100);
-    //     digitalWrite(led_pin,LOW);
-    //   }
-    // }
+        //turn led on and off
+        digitalWrite(led_pin,HIGH);
+        delay(100);
+        digitalWrite(led_pin,LOW);
+        delay(100);
+      }
+    }
   } 
 }
