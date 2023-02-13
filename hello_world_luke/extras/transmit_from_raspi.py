d15f75d6573ec75aa27fb17cc0acf5481b37ed5b
luke-ro <luke.roberson@colorado.edu>
2023-02-13 18:36:13 -0500

    teensy reads float in correctly

--- hello_world_luke/extras/transmit_from_raspi.py
+++ hello_world_luke/extras/transmit_from_raspi.py
@@ -2,22 +2,24 @@
 import serial
 import random
 import time
+import struct
 
 if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
     ser.reset_input_buffer()
     while True:
-        towrite = (input("enter command type to be writtent to teensy \nr: rotate \nt: translate \n s: stop \n m: magnetometer")).encode('utf-8')
-        magnitude =int(input("enter magnitde for command\n"))
-        print(f"Writing \"{towrite}\" to the teensy.") 
-        print("toWrite", towrite)
+        #towrite = (input("enter command type to be writtent to teensy \nr: rotate \nt: translate \n s: stop \n m: magnetometer\n")).encode('utf-8')
+        magnitude =float(input("enter magnitde for command\n"))
+        #print(f"Writing \"{towrite}\" to the teensy.") 
+        #print("toWrite", towrite)
         print("magnitude",magnitude)
-        ser.write(towrite)
+        #ser.write(towrite)
+        ser.write(b"\x01" + struct.pack("<f",magnitude))
         
         
         for i in range(5):
-            time.sleep(1)
-            print(".")
+            while ser.in_waiting > 0:
+                print(ser.readline())
 
         readdata = ser.readline().decode('utf-8').rstrip()
         print(f"The py read \"{readdata}\" from the teensy")
