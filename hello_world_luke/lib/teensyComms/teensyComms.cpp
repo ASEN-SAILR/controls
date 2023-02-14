a97938bb1032b03691b2fb50ad3e3d43e4ad0f0f
skywalkaa <49798726+skywalkaa@users.noreply.github.com>
2023-02-13 17:11:39 -0700

    removed serial prints, readCommand is working?

--- hello_world_luke/lib/teensyComms/teensyComms.cpp
+++ hello_world_luke/lib/teensyComms/teensyComms.cpp
@@ -1,45 +1,27 @@
 #include "teensyComms.h"
 bool teensyComms::readCommand(char* command_type, float* magnitude){
-    // char buffer[32];
-    uint8_t buffer[] = {0x55, 0x66, 0x66, 0x86, 0x40};
+    // buffer to store bytes from serial buffer
+    char buffer[32];
     
 
+    // get the length of the rest of the bytes (should be 4 bytes)
     int msg_len = Serial.available();
 
-    Serial.print("read ");
+    // Get bytes from serial buffer and store in `buffer`
+    Serial.println("Reading in from Pi");
     for(int i=0; i<msg_len; i++){
         buffer[i] = Serial.read();
         Serial.println(buffer[i]);
     }
 
-    Serial.print("Read in: ");
-    
-    // float g = -4.2f;
-    // memcpy(&buffer[1], &g, 4);    // receive data    
+    // The first byte that gets sent is like a 
+    // 2nd byte should be a character that corresponds to a command
+    // TODO: validate the character is correct
+    *command_type = buffer[0];
 
-    for(int i=1; i<5; i++){
-        Serial.println(buffer[i],HEX);
-    }
+    // copy data from buffer and put into float* magnitude
+    memcpy(magnitude, &buffer[1], 4);    // receive data
 
-    float f;
-    memcpy(&f, &buffer[1], 4);    // receive data
-
-    Serial.print("Float: ");
-    Serial.println(f);
-
-    // // Should probably have this read to a new line?
-    // float mag_buffer[] = {0,0,0,0,0,0,0,0};
-    // float magnitude_raw = 0;
-    // float* ptr = &magnitude_raw;
-    
-    // *command_type = char(Serial.read());
-
-    // int mag_length = Serial.available();
-    
-    // //read in bytes of data
-
-    // *magnitude = float(magnitude_raw); 
-    // Serial.println(*magnitude);
     Serial.clear();
     return true;
 }
